1ebd43bc14a434b1fe7f031dfb94249162844c13
alumnodavidfei@gmail.com <hjCDRfdhu142>
2023-02-06 15:44:23 +0100

    commit 9

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,55 @@
+
+function myFunction() {
+
+    var c1 = $("#curso1").val();
+    var c2= $("#tema1").val();
+    var search = {
+	   'curso':c1,
+	   'tema':c2
+			};
+
+	   $.ajax({
+	        type: 'POST',
+	        dataType: 'json',
+	        url: 'http://127.0.0.1:8080/add',
+	       	data: search,
+
+	  			     success: function (data) {
+
+			         console.log("SUCCESS : ", data);
+			       			},
+			        error: function (e) {
+						 console.log("ERROR : ", e);
+			        }
+	    });
+
+angular.element('#mycontroller').scope().ButtonClick();
+
+}
+
+
+//with onclick
+//handleClick(event: Event) {
+/*
+var app = angular.module('myApp', []);
+    app.controller('customersCtrl', function($scope, $http) {
+	//$scope.myFunc = function() {
+        $http.get("http://127.0.0.1:8080/all").then(function (success) {
+            //$scope.myData = response.data.records;
+            $scope.myData = success.data;
+            //console.log(myData);
+        });
+	//	};
+    });
+*/
+ var app = angular.module('myApp', [])
+    app.controller('customersCtrl', function ($scope, $http, $window) {
+        $scope.ButtonClick = function () {
+              $http.get("http://127.0.0.1:8080/all").then(function (success) {
+                      //$scope.myData = response.data.records;
+                      $scope.myData = success.data;
+
+                  });
+                         }
+                    });
+
---
+++ src/test/java/org/example/controller/MainControllerTest.java
@@ -0,0 +1,42 @@
+package org.example.controller;
+
+import org.example.dto.Curso;
+import org.example.repository.UserRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+    @MockBean
+    private UserRepository userRepository;
+
+
+    @Test
+    public void getCursos() throws Exception {
+
+        Curso curso = new Curso();
+        curso.setId(1);
+        given(userRepository.findById(1)).willReturn(Optional.of(curso));
+        mvc.perform(get("/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tema").value("aa"));
+     }
+}

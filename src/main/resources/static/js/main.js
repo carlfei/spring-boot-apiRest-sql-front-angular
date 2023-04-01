d676d316f35c97394fe2184e69077c7cf206e4f2
David <49040356+carlfei@users.noreply.github.com>
2023-04-01 16:52:04 +0200

    Update main.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,6 @@
 
 function myFunction() {
-
+   
     var c1 = $("#curso1").val();
     var c2= $("#tema1").val();
     var search = {
@@ -27,21 +27,6 @@ angular.element('#mycontroller').scope().ButtonClick();
 
 }
 
-
-//with onclick
-//handleClick(event: Event) {
-/*
-var app = angular.module('myApp', []);
-    app.controller('customersCtrl', function($scope, $http) {
-	//$scope.myFunc = function() {
-        $http.get("http://127.0.0.1:8080/all").then(function (success) {
-            //$scope.myData = response.data.records;
-            $scope.myData = success.data;
-            //console.log(myData);
-        });
-	//	};
-    });
-*/
  var app = angular.module('myApp', [])
     app.controller('customersCtrl', function ($scope, $http, $window) {
         $scope.ButtonClick = function () {
